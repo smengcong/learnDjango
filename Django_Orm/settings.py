b3a83c38ac134e47ecc8457783e2480599b42555
Tracy <767069088@qq.com>
2018-07-05 16:47:58 +0800

    增加uwsgi配置文件 以及提取admin样式文件

--- Django_Orm/settings.py
+++ Django_Orm/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 #对数据库进行操作时会显示sql语句日志
 # LOGGING = {
